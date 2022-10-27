9469215f4628eaa0d65b412d43cbb33e799a8cf6
Luiz Henrique Serafim <luizhserafim2001@hotmail.com>
2022-10-27 00:51:59 -0300

    Atualizei o arquivo AssisistenteAdministrativo

--- sistema_biblioteca/AssistenteAdministrativo.java
+++ sistema_biblioteca/AssistenteAdministrativo.java
@@ -3,8 +3,31 @@ package sistema_biblioteca;
 public class AssistenteAdministrativo extends Usuario{
 
     private String inicioContrato;
+    private String finalContrato;
 
-    AssistenteAdministrativo(int CodigoUsuario, String Nome, String Cpf, String Email){
+    AssistenteAdministrativo(int CodigoUsuario, String Nome, String Cpf, String Email, String inicioContrato, String finalContrato){
         super(CodigoUsuario, Nome, Cpf, Email);
+        this.inicioContrato = inicioContrato;
+        this.finalContrato = finalContrato;
+    }
+
+    public String getInicioContrato(){
+        return inicioContrato;
+    }
+
+    public void setInicioContrato(String inicioContrato){
+        this.inicioContrato = inicioContrato;
+    }
+    
+    public String getFinalContrato(){
+        return finalContrato;
+    }
+
+    public void setFinalContrato(String finalContrato){
+        this.finalContrato = finalContrato;
+    }
+    
+    public String toString(){
+        return "Assistente Administrativo encontrado!";
     }
 }
