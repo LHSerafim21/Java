1339e2b18c93251f42b095fdca588b3b89f39901
Luiz Henrique Serafim <luizhserafim2001@hotmail.com>
2022-11-06 00:31:52 -0300

    Geters e Seters atualizados

--- sistema_imobiliaria/Imobiliaria.java
+++ sistema_imobiliaria/Imobiliaria.java
@@ -12,11 +12,89 @@ public class Imobiliaria {
     private ArrayList<Seguro> seguros;
     private Configuracao configuracoes;
 
-    Imobiliaria(String nome, String endereco){
+    Imobiliaria(String nome, String endereco, ArrayList<Aluguel> alugueis, ArrayList<Venda> venda, ArrayList<Imovel> imovel, ArrayList<Usuario> clientes, ArrayList<Usuario> corretores, ArrayList<Seguro> seguros, Configuracao configuracoes){
         this.nome = nome;
+        this.endereco = endereco;
+        this.alugueis = alugueis;
+        this.venda = venda;
+        this.imovel = imovel;
+        this.clientes = clientes;
+        this.corretores = corretores;
+        this.seguros = seguros;
+        this.configuracoes = configuracoes;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getEndereco(){
+        return endereco;
+    }
+
+    public void setEndereco(String endereco){
         this.endereco = endereco;
     }
 
-    //TODO - Analisar adicao de possiveis atributos de controle e adicionar os metodos necessarios + construtores + getters and setters
+    public ArrayList<Aluguel> getAlugueis(){
+        return alugueis;
+    }
+
+    public void setAlugueis(ArrayList<Aluguel> alugueis){
+        this.alugueis = alugueis;
+    }
+
+    public ArrayList<Venda> getVenda(){
+        return venda;
+    }
+
+    public void setVenda(ArrayList<Venda> venda){
+        this.venda = venda;
+    }
+
+    public ArrayList<Imovel> getImovel(){
+        return imovel;
+    }
+
+    public void setImovel(ArrayList<Imovel> imovel){
+        this.imovel = imovel;
+    }
+
+    public ArrayList<Usuario> getClientes(){
+        return clientes;
+    }
+
+    public void setClientes(ArrayList<Usuario> clientes){
+        this.clientes = clientes;
+    }
+
+    public ArrayList<Usuario> getCorretores(){
+        return corretores;
+    }
+
+    public void setCorretores(ArrayList<Usuario> corretores){
+        this.corretores = corretores;
+    }
+
+    public ArrayList<Seguro> getSeguros(){
+        return seguros;
+    }
+
+    public void setSeguros(ArrayList<Seguro> seguros){
+        this.seguros = seguros;
+    }
+
+    public Configuracao getConfiguracoes(){
+        return configuracoes;
+    }
+
+    public void setConfiguracoes(Configuracao configuracoes){
+        this.configuracoes = configuracoes;
+    }
+    //Analisar adicao de possiveis atributos de controle e adicionar os metodos necessarios 
 
 }
